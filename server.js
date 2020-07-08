ebc44f88c22f978e97d46493a5392b9044a31a7b
Kevin <kgilloo1@friars.providence.edu>
2020-07-08 14:26:05 -0400

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const buildPath = path.resolve(__dirname, './build');
 const port = process.env.PORT || 5000;
 require ('dotenv').config();
 
