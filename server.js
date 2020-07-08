07d3a0c5667e7a5e6b1e25da7c54bda66c99d2cf
Kevin <kgilloo1@friars.providence.edu>
2020-07-07 21:32:59 -0400

    changed name of app to server in server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ server.get("*", (req, res) => {
   res.sendFile("index.html", { root });
 });
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
+server.use('/static', express.static(path.join(__dirname, 'public')))
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
