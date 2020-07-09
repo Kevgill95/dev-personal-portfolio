fd2ee3b83aeca0ca0ee5ca07ee4860c2a7e0da3d
Kevin <kgilloo1@friars.providence.edu>
2020-07-09 00:52:00 -0400

    big change, file structures changing

---
+++ build/precache-manifest.0dd503b0e6e57f0ec236fce3cd39fbcd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34c81d34116549fe67a04a1e3ad6306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf09f7be925d79e2af63",
+    "url": "/static/css/2.4e1b8863.chunk.css"
+  },
+  {
+    "revision": "4ab350cb1b13f0b31ff6",
+    "url": "/static/css/main.543bf0cf.chunk.css"
+  },
+  {
+    "revision": "bf09f7be925d79e2af63",
+    "url": "/static/js/2.d3f6ca97.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.d3f6ca97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab350cb1b13f0b31ff6",
+    "url": "/static/js/main.240a7934.chunk.js"
+  },
+  {
+    "revision": "37861d1ca4cf275cf4af",
+    "url": "/static/js/runtime-main.c1520d8d.js"
+  },
+  {
+    "revision": "98c3dc623cfbe08cffe85ce3c2fe60f0",
+    "url": "/static/media/Kevin_Lily.98c3dc62.jpg"
+  },
+  {
+    "revision": "656db3391fddf8230baaf79af7f00f44",
+    "url": "/static/media/rsz_kev.656db339.jpg"
+  }
+]);
